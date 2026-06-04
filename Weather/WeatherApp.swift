fce5c44eda0825931eacc3e140590a4bc392efa6
ankthba <anikethdb1@gmail.com>
2026-06-04 14:21:01 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Aniketh Bandlamudi on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
